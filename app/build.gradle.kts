57360eb39ac9c8db43a409f2fa11008789e4de37
Sajib Bin Mamun| <82803605+SajibMamun@users.noreply.github.com>
2023-12-10 16:23:44 +0600

    Activity Main Done and Initialize all the Sensor

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     defaultConfig {
         applicationId = "com.jantawifi.sensorreader"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
@@ -29,6 +29,10 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -39,4 +43,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
+    implementation("androidx.cardview:cardview:1.0.0")
+
 }
--- app/src/main/java/com/jantawifi/sensorreader/MainActivity.java
+++ app/src/main/java/com/jantawifi/sensorreader/MainActivity.java
@@ -2,13 +2,53 @@ package com.jantawifi.sensorreader;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toolbar;
+
+import com.jantawifi.sensorreader.R;
+import com.jantawifi.sensorreader.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    private TextView lightValue, proximityValue, accelerometerValue, gyroscopeValue;
+    private SensorManager sensorManager;
+    private Sensor lightSensor, proximitySensor, accelerometerSensor, gyroscopeSensor;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setSupportActionBar(binding.toolbar);
+
+        // Enable the Up button
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+
+        lightValue = binding.lightValue;
+        proximityValue = binding.proximityValue;
+        accelerometerValue = binding.accelerometerValue;
+        gyroscopeValue = binding.gyroscopeValue;
+
+        // Initialize sensors
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        gyroscopeSensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+
     }
+
 }
